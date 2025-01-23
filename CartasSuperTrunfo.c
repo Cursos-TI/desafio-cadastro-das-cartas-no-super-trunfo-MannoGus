bcc9771a5ed78c188e8da46ce266a7bdc6b6eb52
MannoGus <gustavo_tio@hotmail.com>
2025-01-23 12:28:32 -0300

    Update CartasSuperTrunfo.c
    criado toda a parte de coleta e mostragem de dados da carta, com algumas linhas de codigo para vericação de dados digitados pelo usuario para nao ser amazenados em locais errados.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,70 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    int  população, p_turistico, n_carta;
-    float area, PIB, 
-    char nome[40], l_carta,
-    
-    // Cadastro das Cartas:  
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+int main(void){
+//variaveis de entrada para armazenar dados das cartas//
 
-    printf("------------SUPER TRUNFO PAISES---------------");
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+int populaçao, n_carta, p_turistico; //variavel int para armazer numeros inteiros//
+double PIB, area; //variavel float para armazenar numeros de ponto flutuantes//
+char l_carta, nome[50], confirmaçao; //variavel char para armazenar letras//
+
+
+do {
+        printf("DIGITE O NOME DA CIDADE:\n");
+        scanf("%[^\n]", &nome);  //%[^\n]usado para ler a quebra de linha ocasionada pelo espaço//
+
+            printf("DIGITE A POPULAÇÃO DA CIDADE:\n");
+            scanf(" %d", &populaçao);
+
+        printf("DIGITE A AREA DA CIDADE:\n");
+        scanf(" %lf", &area);
+
+            printf("DIGITE O PIB DA CIDADE:\n");
+            scanf(" %lf", &PIB);
+
+        printf("DIGITE A QUANTIDADES DE PONTOS TURISTICOS DA CIDADE:\n");
+        scanf(" %i", &p_turistico);
+
+        do {
+            printf("DIGITE O CODIGO DA CARTA LETRA (A-H), SEGUIDA DE UM NUMERO (1-4):\n");
+            scanf(" %c%d", &l_carta, &n_carta);
+
+             if ((l_carta < 'A' || l_carta > 'H') || (n_carta < 1 || n_carta > 4)) {
+                printf("Entrada invalida! Por favor, digite uma letra entre A e H e um numero entre 1 e 4.\n");
+            }
+
+        } while (l_carta < 'A' || l_carta > 'H' || n_carta < 1 || n_carta > 4);
+
+            printf("---------------CONFIRMAÇÃO DE DADOS INSERIDOS-------------\n");
+
+            printf("NOME DA CIDADE: %s\n", nome);
+            printf("POPULAÇÃO: %i\n", populaçao);
+            printf("AREA: %.2fKM\n", area);
+            printf("PIB: %.2f$\n", PIB);
+            printf("PONTOS TURISTICOS: %i\n", p_turistico);
+            printf("CODIGO DA CARTA: %c%i\n", l_carta, n_carta);
+
+            printf("---------PARA CONFIRMAR DIGITE 'S' / PARA DIGITAR NIVAMENTE DIGITE 'N'-----------\n");
+            scanf(" %c", &confirmaçao);
+
+
+} while (confirmaçao != 's' && confirmaçao != 'S');
+
+    printf("DADOS CONFIRMADOS COM SUCESSO\n");
+
+ printf("--------------- FIM DO PROGRAMA -------------\n");
+ 
+
+ printf("ESTE PROGRAMA FOI DESENVOLVIDO PELO ALUNO: GUSTAVO MORAIS\n");
+                printf("--------------- CURSO: JOGOS DIGITAIS -------------\n");
+                printf("--------------- DISCIPLINA: INTRODUÇÃO À PROGRAMAÇÃO DE COMPUTADORES -------------\n");
+                printf("--------------- FACULDADE: ESTACIO -------------\n");
+
+ printf("--------------- PARA FINALIZAR PRECIONE QUALQUER TECLA DO SEU TECLADO -------------\n");
+                getchar();
+
+
 
     return 0;
+
 }
