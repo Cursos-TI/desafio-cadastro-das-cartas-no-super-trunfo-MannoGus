c53949351c6d96adf1c1ae3c1e1e79b06e54f123
MannoGus <gustavo_tio@hotmail.com>
2025-02-04 02:36:47 +0000

    #nova biblioteca adicionada e novos comandos adicionado #nova estrutura para armazenar cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,70 +1,120 @@
 #include <stdio.h>
+#include <string.h>
+
+struct carta {
+    int populaçao, n_carta, p_turistico; //variavel int para armazer numeros inteiros//
+double PIB, area, densidade_populacional, pib_capital; //variavel double para armazenar numeros de ponto flutuantes e numeros inteiros//
+char l_carta, nome[50]; //variavel char para armazenar letras//
+
+};
 
 
-int main(void){
-//variaveis de entrada para armazenar dados das cartas//
+int main(void) {
 
-int populaçao, n_carta, p_turistico; //variavel int para armazer numeros inteiros//
-double PIB, area; //variavel float para armazenar numeros de ponto flutuantes//
-char l_carta, nome[50], confirmaçao; //variavel char para armazenar letras//
+    char confirmaçao; //variavel char para armazenar letras//
+ struct carta carta1; //estrutura para armazenar os dados da carta//
 
 
-do {
+    printf("---------------BEM VINDO AO JOGO SUPER TRUNFO-------------\n");
+    printf("---------------VAMOS INICIAR A CRIAÇÃO DA CARTA (01)-------------\n");
+ 
+do { //laço de repetição para confirmar os dados inseridos//
         printf("DIGITE O NOME DA CIDADE:\n");
-        scanf("%[^\n]", &nome);  //%[^\n]usado para ler a quebra de linha ocasionada pelo espaço//
+        scanf("%[^\n]", &carta1.nome);  //%[^\n]usado para ler a quebra de linha ocasionada pelo espaço//
 
             printf("DIGITE A POPULAÇÃO DA CIDADE:\n");
-            scanf(" %d", &populaçao);
+            scanf(" %d", &carta1.populaçao);
 
         printf("DIGITE A AREA DA CIDADE:\n");
-        scanf(" %lf", &area);
+        scanf(" %lf", &carta1.area);
 
             printf("DIGITE O PIB DA CIDADE:\n");
-            scanf(" %lf", &PIB);
+            scanf(" %lf", &carta1.PIB);
 
         printf("DIGITE A QUANTIDADES DE PONTOS TURISTICOS DA CIDADE:\n");
-        scanf(" %i", &p_turistico);
+        scanf(" %i", &carta1.p_turistico);
 
-        do {
+        do { //laço de repetição para confirmar os dados inseridos//
             printf("DIGITE O CODIGO DA CARTA LETRA (A-H), SEGUIDA DE UM NUMERO (1-4):\n");
-            scanf(" %c%d", &l_carta, &n_carta);
+            scanf(" %c%d", &carta1.l_carta, &carta1.n_carta);
 
-             if ((l_carta < 'A' || l_carta > 'H') || (n_carta < 1 || n_carta > 4)) {
+             if ((carta1.l_carta < 'A' || carta1.l_carta > 'H') || (carta1.n_carta < 1 || carta1.n_carta > 4)) { 
+                //condição para verificar se a letra digitada esta entre A e H e se o numero digitado esta entre 1 e 4//
                 printf("Entrada invalida! Por favor, digite uma letra entre A e H e um numero entre 1 e 4.\n");
             }
 
-        } while (l_carta < 'A' || l_carta > 'H' || n_carta < 1 || n_carta > 4);
+        } while (carta1.l_carta < 'A' || carta1.l_carta > 'H' || carta1.n_carta < 1 || carta1.n_carta > 4);
 
-            printf("---------------CONFIRMAÇÃO DE DADOS INSERIDOS-------------\n");
+            printf("---------------CONFIRMAÇÃO DE DADOS INSERIDOS DA CARTA (01)-------------\n");
 
-            printf("NOME DA CIDADE: %s\n", nome);
-            printf("POPULAÇÃO: %i\n", populaçao);
-            printf("AREA: %.2fKM\n", area);
-            printf("PIB: %.2f$\n", PIB);
-            printf("PONTOS TURISTICOS: %i\n", p_turistico);
-            printf("CODIGO DA CARTA: %c%i\n", l_carta, n_carta);
-
-            printf("---------PARA CONFIRMAR DIGITE 'S' / PARA DIGITAR NIVAMENTE DIGITE 'N'-----------\n");
+            printf("NOME DA CIDADE: %s\n", carta1.nome);
+            printf("POPULAÇÃO: %i\n", carta1.populaçao);
+            printf("AREA: %.2fKM\n", carta1.area);
+            printf("PIB: %.2f$\n", carta1.PIB);
+            printf("PONTOS TURISTICOS: %i\n", carta1.p_turistico);
+            printf("CODIGO DA CARTA: %c%i\n", carta1.l_carta, carta1.n_carta);
+//condiçao para confirmar os dados inseridos//
+            printf("---------PARA CONFIRMAR DIGITE 'S' / PARA DIGITAR NOVAMENTE DIGITE 'N'-----------\n");
             scanf(" %c", &confirmaçao);
 
 
 } while (confirmaçao != 's' && confirmaçao != 'S');
 
-    printf("DADOS CONFIRMADOS COM SUCESSO\n");
+    //calculos para densidade populacional e PIB per capita//
+    carta1.densidade_populacional = carta1.populaçao / carta1.area; //formula para calcular a densidade populacional//
+    carta1.pib_capital = carta1.PIB / carta1.populaçao; //formula para calcular o PIB per capita//
 
+
+    printf("-------TODAS INFORMAÇOES DA CARTA (01)-------\n");
+        printf("NOME DA CIDADE: %s\n", carta1.nome);
+        printf("POPULAÇAO: %i\n", carta1.populaçao);
+        printf("AREA: %.2fKM\n", carta1.area);
+        printf("DENSIDADE POPULACIONAL: %.2f PESSOAS/KM²\n", carta1.densidade_populacional);
+
+//condiçoes para exibir o PIB em trilhoes, bilhoes, milhoes e reais//
+            if (carta1.PIB >= 1e12) {
+                printf("PIB: %.2f TRILHOES DE REAIS\n", carta1.PIB / 1e12);
+            } else if (carta1.PIB >= 1e9){
+                printf("PIB: %.2f BILHÕES DE REAIS\n", carta1.PIB / 1e9);
+            } else if (carta1.PIB >= 1e6){
+                printf("PIB: %.2f MILHÕES DE REAIS\n", carta1.PIB / 1e6);
+            } else {
+                printf("PIB: %.2f REAIS\n",carta1.PIB);
+            }
+
+            printf("PIB PER CAPITA: %.2f$\n", carta1.pib_capital);
+            printf("PONTOS TURISTICOS: %i\n", carta1.p_turistico);
+            printf("CODIGO DA CARTA: %c%i\n", carta1.l_carta, carta1.n_carta);
+            
+                    
+//------------------------------------------------------------------------------------------------------------//
+                         //ESCRITA DO PROGRAMA FINALIZADA AQUI OS COMANDOS LOGO ABAIXO//
+                                         //E APENAS PARA INFORMOCÕES//
+//-----------------------------------------------------------------------------------------------------------//
+
+
+ printf("\n"); //espaço para separar as informações//
+ printf("\n"); //espaço para separar as informações//
  printf("--------------- FIM DO PROGRAMA -------------\n");
+ printf("\n"); //espaço para separar as informações//
+ printf("\n"); //espaço para separar as informações//
  
 
- printf("ESTE PROGRAMA FOI DESENVOLVIDO PELO ALUNO: GUSTAVO MORAIS\n");
+ printf("--------------- ESTE PROGRAMA FOI DESENVOLVIDO PELO ALUNO: GUSTAVO MORAIS ---------------\n");
                 printf("--------------- CURSO: JOGOS DIGITAIS -------------\n");
                 printf("--------------- DISCIPLINA: INTRODUÇÃO À PROGRAMAÇÃO DE COMPUTADORES -------------\n");
                 printf("--------------- FACULDADE: ESTACIO -------------\n");
 
+
+printf("\n");   //espaço para separar as informações//               
+printf("\n");   //espaço para separar as informações//
+
  printf("--------------- PARA FINALIZAR PRECIONE QUALQUER TECLA DO SEU TECLADO -------------\n");
-                getchar();
 
 
+                getchar();  //função para aguardar o usuario pressionar qualquer tecla do teclado//  
+                getchar();  //função para aguardar o usuario pressionar qualquer tecla do teclado//
 
-    return 0;
+return 0;
 
 }
